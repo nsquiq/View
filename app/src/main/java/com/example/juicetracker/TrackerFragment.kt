87cd5e655c5c8cc26b8b039fac0dcc307e64270c
Nuray <210103331@stu.sdu.edu.kz>
2023-12-09 09:26:55 +0600

    codelab1

--- app/src/main/java/com/example/juicetracker/TrackerFragment.kt
+++ app/src/main/java/com/example/juicetracker/TrackerFragment.kt
@@ -13,6 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.juicetracker
 
 import android.os.Bundle
@@ -24,6 +39,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.juicetracker.databinding.FragmentTrackerBinding
 import com.example.juicetracker.ui.AppViewModelProvider
 import com.example.juicetracker.ui.JuiceListAdapter
@@ -44,7 +61,9 @@ class TrackerFragment : Fragment() {
 
     private val adapter = JuiceListAdapter(
         onEdit = { drink ->
-
+            findNavController().navigate(
+                TrackerFragmentDirections.actionTrackerFragmentToEntryDialogFragment(drink.id)
+            )
         },
         onDelete = { drink ->
             viewModel.deleteJuice(drink)
@@ -56,7 +75,9 @@ class TrackerFragment : Fragment() {
         binding.recyclerView.adapter = adapter
 
         binding.fab.setOnClickListener { fabView ->
-
+            fabView.findNavController().navigate(
+                TrackerFragmentDirections.actionTrackerFragmentToEntryDialogFragment()
+            )
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
